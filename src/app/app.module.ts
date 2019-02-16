cb116ba424760ef851b5e91839a67fe909e0c1b7
Timothy Ngorima <timothytakudzwa@gmail.com>
2019-02-16 11:07:08 +0200

    Adding New Pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
+
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
@@ -12,15 +11,24 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { SignupPage } from '../pages/signup/signup';
 import { LoginPage } from '../pages/login/login';
+import { AlertsPage } from '../pages/alerts/alerts';
+import { EmergencyPage } from '../pages/emergency/emergency';
+import { ChatPage } from '../pages/chat/chat';
+import { ProfilePage } from '../pages/profile/profile';
+import { NoticePage } from '../pages/notice/notice';
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
+    
     HomePage,
     LoginPage,
     SignupPage,
+    AlertsPage,
+    EmergencyPage,
+    NoticePage,
+    ChatPage,
+    ProfilePage,
     TabsPage
   ],
   imports: [
@@ -30,10 +38,15 @@ import { LoginPage } from '../pages/login/login';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
+  
     LoginPage,
     SignupPage,
-    ContactPage,
+  
+    AlertsPage,
+    NoticePage,
+    EmergencyPage,
+    ChatPage,
+    ProfilePage,
     HomePage,
     TabsPage
   ],
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+import { AlertsPage } from '../alerts/alerts';
+import { ChatPage } from '../chat/chat';
+import { EmergencyPage } from '../emergency/emergency';
+import { ProfilePage } from '../profile/profile';
+import { NoticePage } from '../notice/notice';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +13,53 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
 
   }
 
+  viewAlerts() {
+    this.navCtrl.push(AlertsPage)
+  }
+
+  chat() {
+    this.navCtrl.push(ChatPage)
+  }
+
+  emergency() {
+    this.navCtrl.push(EmergencyPage)
+  }
+  profile() {
+    this.navCtrl.push(ProfilePage)
+  }
+  notice() {
+    this.navCtrl.push(NoticePage)
+  }
+
+  makeCall() {
+    const confirm = this.alertCtrl.create({
+      title: 'Call Night Guard',
+      message: 'Please Select the Guard you want to call. By clicking the call button, the call will be redirected to the select guard and normal call tarrifs apply.',
+      buttons: [
+        {
+          text: 'Call Guard 1',
+          handler: () => {
+            console.log('Disagree clicked');
+          }
+        },
+        {
+          text: 'Call Guard 2',
+          handler: () => {
+            console.log('Agree clicked');
+          }
+        },
+        {
+          text: 'Call Guard 3',
+          handler: () => {
+            console.log('Agree clicked');
+          }
+        }
+      ]
+    });
+    confirm.present();
+  }
 }
